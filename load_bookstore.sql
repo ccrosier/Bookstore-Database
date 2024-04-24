8af618e49f3aa7a5db56c674679919a670859804
pc236 <143962365+pc236@users.noreply.github.com>
2024-04-24 18:23:33 -0400

    Update load_bookstore.sql
    Changed one index to match database changes.

--- load_bookstore.sql
+++ load_bookstore.sql
@@ -36,12 +36,12 @@ CREATE INDEX author_and_title
 ON BOOK (AID, Title);
 
 /*
-* Second index: find the phone number of a party the user wishes to contact, categorized by the ForeignID, E-mail and phone number. Reasoning: this index has been chosen in the case that the user wishes to make contact with the party (perhaps uses the phone number
-* to make immediate contact, then use the e-mail address as a follow-up). Including the address didn't seem immedaitely pertinent, since most users would not be writing on paper directly to the party they wish to communicate with.
+* Second index: find the phone number of a publisher the user wishes to contact, categorized by PID, E-mail and phone number. Reasoning: this index has been chosen in the case that the user wishes to make contact with the publisher (perhaps uses the phone number
+* to make immediate contact, then use the e-mail address as a follow-up). Including the address didn't seem immedaitely pertinent, since most users would not be writing on paper directly to communicate with the publisher.
 */
 
-CREATE INDEX contact_information
-ON CONTACT (ForeignID, Email, Phone);
+CREATE INDEX pub_contact_information
+ON PUBLISHER (PID, Email, Phone);
 
 /*
 * Third index: find review scores for books by BISBN, Rating (from highest to lowest) and Content. Reasoning: if a user wishes to simply filter results for reviews of "good" books, they can try to find the highest rated books and read the corresponding for them.
