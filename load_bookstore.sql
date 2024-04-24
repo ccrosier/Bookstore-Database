127d82befa3f3dc0367b9e3f5fa7eaca2ddbdc55
pc236 <143962365+pc236@users.noreply.github.com>
2024-04-24 18:04:00 -0400

    Update load_bookstore.sql
    Providing indices for querying schema within the database.

--- load_bookstore.sql
+++ load_bookstore.sql
@@ -26,3 +26,27 @@ create view Top5Genre as select Category, quant as QuantitySold from (
     where b.ISBN=p.BISBN 
     group by b.Category) 
     order by quant desc limit 5;
+
+/*
+* First index: find books categorized by Author and Title. Reasoning: end users will often rarely use raw SQL and do not see the indices, but may be searching for something within the schema that's very common and gives key information.
+* It makes sense to think of books in terms of the author and the title, hence making an index for AID and Title would be a good candidate index.
+*/
+
+CREATE INDEX author_and_title
+ON BOOK (AID, Title);
+
+/*
+* Second index: find the phone number of a party the user wishes to contact, categorized by the ForeignID, E-mail and phone number. Reasoning: this index has been chosen in the case that the user wishes to make contact with the party (perhaps uses the phone number
+* to make immediate contact, then use the e-mail address as a follow-up). Including the address didn't seem immedaitely pertinent, since most users would not be writing on paper directly to the party they wish to communicate with.
+*/
+
+CREATE INDEX contact_information
+ON CONTACT (ForeignID, Email, Phone);
+
+/*
+* Third index: find review scores for books by BISBN, Rating (from highest to lowest) and Content. Reasoning: if a user wishes to simply filter results for reviews of "good" books, they can try to find the highest rated books and read the corresponding for them.
+* This makes it easy for the user to find which books are popular and may be worth reading.
+*/
+
+CREATE INDEX book_reviews
+ON REVIEW (BISBN, Rating DESC, Content);
